b14c3392ed15451082853ee29df2d49f02e70116
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-02-20 17:31:15 +0000

    adding about component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailsComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -49,6 +50,7 @@ class Main extends Component {
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
+            <Route exact path='/aboutus' component={() => <About leaders = {this.state.leaders}/> }/>
             <Redirect to="/home" />
           </Switch>
         <Footer />
